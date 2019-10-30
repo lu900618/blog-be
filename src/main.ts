aa75af03b0199d13d1c55bcb919a5b9a039d6224
lu900618 <32401545@qq.com>
2019-10-30 17:40:14 +0800

    配置自动生成api文档

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 生成api文档
+  const options = new DocumentBuilder()
+    .setTitle('博客API')
+    .setDescription('The blog API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

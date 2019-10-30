ac735324d09c1c2fb04a136c482fab9618b81d25
lu900618 <32401545@qq.com>
2019-10-30 17:43:05 +0800

    连接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
+import { TypegooseModule } from 'nestjs-typegoose'
 
 @Module({
-  imports: [LoginModule],
+  imports: [
+    TypegooseModule.forRoot("mongodb://localhost:27017/blog", {
+      useNewUrlParser: true
+    }),
+    LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }

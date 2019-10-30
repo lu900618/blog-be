4c951cc51e373b4b119c7311c49a2e16d753f956
lu900618 <32401545@qq.com>
2019-10-30 18:44:13 +0800

    封装提示信息

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,9 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.useGlobalFilters(new HttpExceptionFilter()); // 拦截全部的错误请求,统一返回格式
+  app.useGlobalInterceptors(new TransformInterceptor()); // 统一请求成功的返回数据
 
   await app.listen(3000);
 }
